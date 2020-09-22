a3fefb804c46f42c2196cde837bbb206e8c0b8c0
Alécio <alecio.godoi@ymail.com>
2020-09-22 11:10:04 -0300

    Aula 05 - Projetos - JPA-02 e JPA03

---
+++ JPA_02/src/com/eclipselink/entity/Cliente.java
@@ -0,0 +1,79 @@
+package com.eclipselink.entity;
+
+import java.io.Serializable;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+
+
+@Entity
+@Table
+@Inheritance( strategy = InheritanceType.SINGLE_TABLE )
+@DiscriminatorColumn( name = "TIPO" )
+public class Cliente implements Serializable {
+	
+	@GeneratedValue( strategy = GenerationType.AUTO )
+
+	@Id
+	private int idCliente;
+	
+	private String nome;
+	private String endereco;
+	private String telefone;
+	private String email;
+	
+	public Cliente() {
+	}
+	
+	public Cliente(int idCliente, String nome, String endereco, String telefone, String email) {
+		super();
+		this.idCliente = idCliente;
+		this.nome = nome;
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.email = email;
+	}
+	
+	public int getIdCliente() {
+		return idCliente;
+	}
+	public void setIdCliente(int idCliente) {
+		this.idCliente = idCliente;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Cliente [idCliente=" + idCliente + ", nome=" + nome + ", endereco=" + endereco + ", telefone="
+				+ telefone + ", email=" + email + "]";
+	}
+
+}
